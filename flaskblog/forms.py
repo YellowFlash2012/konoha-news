2e3e1b2a773da8291254f9319a421893cfdd836b
YellowFlash2012 <cyclop9@hotmail.com>
2021-11-03 16:03:45 +0100

    create, update and delete posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed 
 from wtforms import StringField
 from wtforms.fields.core import BooleanField
-from wtforms.fields.simple import PasswordField, SubmitField
+from wtforms.fields.simple import PasswordField, SubmitField, TextAreaField
 from wtforms.validators import *
+from wtforms.widgets.core import TextArea
 
 from flaskblog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -37,3 +40,34 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(
+        min=2, max=20)], render_kw={'autofocus': True})
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    # to prevent the same username being used twice
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'That email is already taken. Please use another one')
+
+    # to prevent the same email being used twice
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'That username is already taken. Please choose another one')
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,11 +1,13 @@
-from flask import render_template, url_for, flash, request
+import secrets
+import os
+from PIL import Image
+
+from flask import render_template, url_for, flash, request, abort
 from flask_login.utils import login_required
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user
-
-from .posts import *
 
 from werkzeug.utils import redirect
 
@@ -13,6 +15,7 @@ from werkzeug.utils import redirect
 @app.route('/')
 @app.route('/home')
 def home():
+    posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
 
@@ -62,8 +65,90 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-@app.route('/account')
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', ttitle='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account')) #to avid the weird "confirm form resubmission pop up message"
+    #prepopulate form fields with current_user data
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        new_post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(new_post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='Update post')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('post.html', title=post.title, post=post, image_file=image_file)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+        flash('You can NOT edit a post you did NOT create', 'danger')
+    
+    form=PostForm()
+    
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been update', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update post', form=form, legend='Update post')
+
+
+@app.route('/post/<int:post_id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Post sucessfully deleted', 'success')
+    return redirect(url_for('home'))
